8ef05a3d87ed46f0e59dda9756cc175bd9d87ac2
Robert Juall <RJuall@gmail.com>
2019-02-15 01:20:51 -0500

    Add reactstrap, create 3 columns

--- app/src/components/App.jsx
+++ app/src/components/App.jsx
@@ -14,12 +14,26 @@ export class App extends React.Component{
 
     render(){
         return (
-            <div>
-                Hey, it works.
-                <br></br>
-                Can you see me? If so, you've got FontAwesome Pro! <FontAwesomeIcon size="lg" icon={faAcorn}></FontAwesomeIcon>
-                <br></br>
-                This is from a different FontAwesome icon library! <FontAwesomeIcon size="lg" icon={faApple}></FontAwesomeIcon>
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                    Column 1
+                    </div>
+                    <div class="col-6">
+                        <div>
+                            Column 2
+                            <br></br>
+                            Hey, it works.
+                            <br></br>
+                            Can you see me? If so, you've got FontAwesome Pro! <FontAwesomeIcon size="lg" icon={faAcorn}></FontAwesomeIcon>
+                            <br></br>
+                            This is from a different FontAwesome icon library! <FontAwesomeIcon size="lg" icon={faApple}></FontAwesomeIcon>
+                        </div>
+                    </div>
+                    <div class="col">
+                    Column 3
+                    </div>
+                </div>
             </div>
         );
     }
