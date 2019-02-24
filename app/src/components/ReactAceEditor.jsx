7322f76a48fa25b93a0012edafa4d211db604b59
Robert Juall <RJuall@gmail.com>
2019-02-23 20:52:02 -0500

    Testing editor settings

--- app/src/components/ReactAceEditor.jsx
+++ app/src/components/ReactAceEditor.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import brace from 'brace';
 import AceEditor from 'react-ace';
+import FileDispatcher, { FILE_READ } from '../dispatchers/FileDispatcher';
 
 import 'brace/mode/haskell';
 import 'brace/theme/dracula';
+import 'brace/theme/eclipse';
+import 'brace/theme/solarized_light'
+
+import {testHask} from './Tokenise';
 
 function onChange(newVal) {
-    console.log('change', newVal);
+    console.log();
 }
 
 class ReactAceEditor extends React.Component {
@@ -14,18 +19,26 @@ class ReactAceEditor extends React.Component {
         super(props);
     }
 
+    componentDidMount() {
+        FileDispatcher.on(FILE_READ, evt => {
+            //evt.str to ed.val
+        });
+        console.log(testHask);
+    }    
+
     render() {
         return(
             <div>
                 <AceEditor
                     mode="haskell"
-                    theme="dracula"
+                    theme="solarized_light"
                     onChange={onChange}
                     name="ace-editor"
                     width="100%"
                     height="100vh"
-                    wrapEnabled="true"
+                    fontSize="22px"
                     editorProps={{$blockScrolling: true}}
+                    defaultValue={testHask}
                 ></AceEditor>
             </div>
         )
