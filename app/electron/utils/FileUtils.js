e77317ccffb2afa8982f34ea6a614066d06bea77
David <david@rosenblum.net>
2019-02-24 12:44:29 -0500

    Main process new file creation

--- app/electron/utils/FileUtils.js
+++ app/electron/utils/FileUtils.js
@@ -3,26 +3,54 @@ const fs = require("fs");
 // file system calls (promise wrappers)
 class FileUtils{
     // writes a file asychronously
-    // @param fname file name to write
+    // @param path  file path to write
     // @param str   string to write to the file 
-    static writeFile(fname, str){
+    static writeFile(path, str){
+        console.log('fu write', str);
         return new Promise((resolve, reject) => {
-            fs.writeFile(fname, str, err => {
+            fs.writeFile(path, str, err => {
                 err ? reject(err) : resolve("Success.");
             });
         });
     }
 
     // reads a file asychronously 
-    // @param fname     filen name to read
-    static readFile(fname){
+    // @param path      filen path to read
+    static readFile(path){
         return new Promise((resolve, reject) => {
-            fs.readFile(fname, (err, buf) => {
+            fs.readFile(path, (err, buf) => {
                 if(!err){
                     resolve(buf.toString());
                 }
                 else reject(err);
             });
+        });
+    }
+
+    // writes a file if it does not exist 
+    // @param path  file to write
+    // @param str   initial file contents (optional)
+    static createFile(path, str=""){
+        return new Promise((resolve, reject) => {
+            // check if file exists
+            this.fileExists(path).then(exists => {
+                if(!exists){
+                    // does not exist - write the file 
+                    console.log('cf', `--${str}--`)
+                    this.writeFile(path, str)
+                        .then(() => resolve("File created."))
+                        .catch(err => reject(err));
+                }
+                else reject(new Error("File already exists."));
+            });
+        });
+    }
+
+    // checks if a file exists
+    // @param path      file path to check 
+    static fileExists(path){
+        return new Promise(resolve => {
+            fs.exists(path, exists => resolve(exists));
         });
     }
 
