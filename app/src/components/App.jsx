813dba875bb613a19c843a2bc714bc0a19ea8a17
David <david@rosenblum.net>
2019-02-24 21:04:47 -0500

    File create modal moved to app

--- app/src/components/App.jsx
+++ app/src/components/App.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { Row, Col, Container } from "reactstrap";
-import { faAcorn } from '@fortawesome/pro-regular-svg-icons';
-import { faApple } from '@fortawesome/free-brands-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import SplitPane from 'react-split-pane';
 import { FileList } from "./FileList";
 import { GhciConsole } from "./GhciConsole";
 import { Editor } from './Editor';
+import { ModalCreateFile } from "./ModalCreateFile";
 import "./App.css";
 
 export class App extends React.Component{
@@ -20,26 +18,29 @@ export class App extends React.Component{
 
     render(){
         return (
-            <Container>
-                <Row>
-                    <SplitPane split="vertical" minSize={100} defaultSize={150}>
-                        <Col>
-                            <FileList/>
-                        </Col>
-                        <div>
-                            <SplitPane split="vertical" minSize={200} defaultSize={1100}>
+            <>
+                <Container>
+                    <Row>
+                        <SplitPane split="vertical" minSize={100} defaultSize={150}>
                             <Col>
-                                <Editor/>
+                                <FileList/>
                             </Col>
-                            <Col>
-                              GHCi
-                              <GhciConsole/>
-                            </Col>
-                            </SplitPane>
-                        </div>
-                    </SplitPane>
-                </Row>
-            </Container>
+                            <div>
+                                <SplitPane split="vertical" minSize={200} defaultSize={1100}>
+                                <Col>
+                                    <Editor/>
+                                </Col>
+                                <Col>
+                                GHCi
+                                <GhciConsole/>
+                                </Col>
+                                </SplitPane>
+                            </div>
+                        </SplitPane>
+                    </Row>
+                </Container>
+                <ModalCreateFile/>
+            </>
         );
     }
 }
