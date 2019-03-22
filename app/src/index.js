38cc84936225a6a28e3808d203f11b354ee68223
David <david@rosenblum.net>
2019-03-21 20:52:06 -0400

    Started context menu

--- app/src/index.js
+++ app/src/index.js
@@ -1,6 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./components/App";
+import ModalDispatcher from "./dispatchers/ModalDispatcher";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
+// import context menu
+const contextMenu = window.require("electron-context-menu");
+
+// setup context menu
+contextMenu({
+    prepend: (params, window) => [
+        {
+            label: "Add Folder",
+            click: () => ModalDispatcher.selectFolderModal()
+        }
+    ],
+    showInspectElement: false
+})
+
+// render app 
 ReactDOM.render(<App/>, document.querySelector("#root"));
