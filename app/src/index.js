c92cc0e9bc29c6b0e7c7817eaa9bb0c885ff2491
David Rosenblum <david@rosenblum.net>
2019-03-23 11:49:42 -0400

    Dynamic context menu

--- app/src/index.js
+++ app/src/index.js
@@ -10,48 +10,80 @@ const contextMenu = window.require("electron-context-menu");
 
 // setup context menu
 contextMenu({
-    prepend: (params, window) => [
-        {
-            label: "Add New File",
-            click: () => ModalDispatcher.selectFileModal()
-        },
-        {
-            label: "Add New Folder",
-            click: () => ModalDispatcher.selectFolderModal()
-        },
-        {
-            label: "Remove Folder",
-            click: () => {
-                // find 'folderPath' attribute of element at (x, y)
-                let folderPath = findElementKeyAt(params.x, params.y, "folderPath");
-                // null = not found
-                if(folderPath){
-                    FolderDispatcher.removeFolder(folderPath);
-                }
+    prepend: (params, window) => {
+        // element clicked
+        let elem = document.elementFromPoint(params.x, params.y);        
+
+        // default menu
+        let prepends = [
+            {
+                label: "Add New File",
+                click: () => ModalDispatcher.selectFileModal()
+            },
+            {
+                label: "Add New Folder",
+                click: () => ModalDispatcher.selectFolderModal()
             }
+        ];
+
+        // folder-only addons
+        if(elementHasFileList(elem)){
+            prepends.push({
+                label: "Remove Folder",
+                click: () => {
+                    // find 'folderPath' attribute of element at (x, y)
+                    findElementKeyAt(params.x, params.y, "folderPath", (elem, folderPath) => {
+                        if(folderPath){
+                            FolderDispatcher.removeFolder(folderPath);
+                        }
+                    });
+                }
+            });
         }
-    ],
+
+        return prepends;
+    },
     showInspectElement: false
 });
 
 // determines the specified html attribute of the context triggering element
+// searches up the DOM to find the first element with the attribute 
 // @param x         x pixel coordinate of element
 // @param y         y pixel coordinate of element
 // @param attrKey   html attribute key 
-let findElementKeyAt = (x, y, attrKey) => {
+// @param callback  resolves the (element, attrKey)
+let findElementKeyAt = (x, y, attrKey, callback) => {
     let elem = document.elementFromPoint(x, y);
+    let value = null;
 
     if(elem){
         do{
             if(elem.getAttribute(attrKey)){
-                return elem.getAttribute(attrKey);
+                value = elem.getAttribute(attrKey);
+                break;
             }
             else{
                 elem = elem.parentNode;
             } 
         } while(elem.parentNode);
     }
-    return null;
+    
+    callback(value ? elem : null, value);
+};
+
+// determines if current element or any parent element is a folder list folder 
+// @param       starting element to check
+let elementHasFileList = elem => {
+    do{
+        if(elem.classList.contains("file-list-folder-container")){
+            return true;
+        }
+        else{
+            elem = elem.parentNode;
+        }
+    } while(elem.parentNode);
+
+    return false;
 };
 
 // render app 
