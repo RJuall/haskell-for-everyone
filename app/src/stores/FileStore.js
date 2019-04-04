4861aca56c1666c56a152cc577d1239d4f10e8ae
David <david@rosenblum.net>
2019-04-03 23:03:58 -0400

    File store recent files

--- app/src/stores/FileStore.js
+++ app/src/stores/FileStore.js
@@ -21,6 +21,20 @@ class FileStore {
         Object.assign(this.fileSettings, this.globalFileSettings);
     }
 
+    recentPathUpdate(path){
+        // concat paths with new path
+        let paths = [...this.fileSettings.recentFilePaths, path];
+
+        // unique paths
+        paths = paths.filter((currPath, idx) => paths.indexOf(currPath) === idx);
+
+        // top 10 most recent
+        paths = paths.slice(0, 10);
+
+        // apply update
+        this.fileSettings.recentFilePaths = paths;
+    }
+
     fileSettingsDisposer = action( autorun( () => {
         IpcRequester.updateFolderData(this.fileSettings);
     }))
