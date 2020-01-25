aa94d2e89926debe542c36cd5a4be85642546fa3
unknown <jtpojero@gmail.com>
2020-01-25 13:56:31 -0500

    Replace buttons are disabled if there is nothing to find

--- app/src/components/SearchBar.jsx
+++ app/src/components/SearchBar.jsx
@@ -6,12 +6,17 @@ import { observer, inject } from 'mobx-react';
 import { action } from 'mobx';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import "./SearchBar.css";
+import { timingSafeEqual } from 'crypto';
 
 export const SearchBar = inject("editorStore", "fileStore","windowStore")(observer(class SearchBar extends React.Component {
     constructor(props) {
         super(props);
 
-        this.findVal = React.createRef();
+        this.state = {
+            findVal: ''
+        };
+
+        //this.findVal = React.createRef();
         this.replaceVal = React.createRef();
         this.handleFindChange = this.handleFindChange.bind(this);
         this.handleReplaceChange = this.handleReplaceChange.bind(this);
@@ -23,7 +28,8 @@ export const SearchBar = inject("editorStore", "fileStore","windowStore")(observ
     }
 
     handleFindChange(evt){
-        this.findVal = evt.target.value;
+        this.setState({findVal : evt.target.value});
+        //this.findVal = evt.target.value;
     }
 
     handleReplaceChange(evt){
@@ -42,8 +48,8 @@ export const SearchBar = inject("editorStore", "fileStore","windowStore")(observ
                     <Label for="find" className="sr-only">Find</Label>
                     <Input className="form-control-sm form-control" type="text" name="find" id="find" placeholder="Find" onChange={this.handleFindChange}/>
                 </FormGroup>
-                <Button className="btn btn-sm ml-2" onClick={() => EditorDispatcher.find(this.findVal,"Next",this.searchType)}>Next</Button>
-                <Button className="btn btn-sm ml-2" onClick={() => EditorDispatcher.find(this.findVal,"Previous",this.searchType)}>Previous</Button>
+                <Button className="btn btn-sm ml-2" onClick={() => EditorDispatcher.find(this.state.findVal,"Next",this.searchType)}>Next</Button>
+                <Button className="btn btn-sm ml-2" onClick={() => EditorDispatcher.find(this.state.findVal,"Previous",this.searchType)}>Previous</Button>
             </Form>
             <button className="escButton" onClick={() => this.closeSearch()}>&#10005;</button>
             <Form inline className="mb-2">
@@ -51,8 +57,8 @@ export const SearchBar = inject("editorStore", "fileStore","windowStore")(observ
                     <Label for="replace" className="sr-only">Replace</Label>
                     <Input className="form-control-sm form-control" type="text" name="replace" id="replace" placeholder="Replace" onChange={this.handleReplaceChange}/>
                 </FormGroup>
-                <Button className="btn btn-sm ml-2" onClick={() => EditorDispatcher.replace(this.replaceVal, "one")}>Replace</Button>
-                <Button className="btn btn-sm ml-2" onClick={() => EditorDispatcher.replace(this.replaceVal, "all")}>Replace All</Button>
+                <Button className="btn btn-sm ml-2" onClick={() => EditorDispatcher.replace(this.replaceVal, "one")} disabled={!this.state.findVal}>Replace</Button>
+                <Button className="btn btn-sm ml-2" onClick={() => EditorDispatcher.replace(this.replaceVal, "all")} disabled={!this.state.findVal}>Replace All</Button>
             </Form>
             <Form inline className="mb-2">
                 <FormGroup>
